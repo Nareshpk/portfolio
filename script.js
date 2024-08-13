dea59e5e5bf6f54da23cca3bdbef86effe63b988
sriramtp <66910258+sriramtp@users.noreply.github.com>
2024-08-14 01:01:54 +0530

    new changes done by Naresh.

--- script.js
+++ script.js
@@ -16,3 +16,45 @@ document.querySelectorAll('a.nav-link').forEach(anchor => {
         });
     });
 });
+
+
+// function sendEmail() {
+//     let param = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         subject: document.getElementById("subject").value,
+//         message: document.getElementById("message").value,
+//     }
+//     console.log("param", param)
+//     emailjs.send("service_79bqyea", "template_8t0a90n", param).then(alert("Email send!!"))
+// }
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+
+    // Collect the form data
+    const templateParams = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    // Send the email
+    emailjs.send("service_79bqyea", "template_8t0a90n", templateParams)
+        .then(function(response) {
+            alert("Email sent successfully!");
+            document.getElementById("contact-form").reset();
+        }, function(error) {
+            alert("Failed to send email: " + error.text);
+        });
+});
+
+
+document.getElementById("viewResumeBtn").addEventListener("click", function() {
+    // Update this URL to the location of your PDF file
+    const pdfUrl = "images/Nareshkumar.pdf";
+
+    // Open the PDF in a new window or tab
+    window.open(pdfUrl, '_blank');
+});
